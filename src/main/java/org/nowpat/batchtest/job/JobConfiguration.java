5b01b6931469e705dff173c45437b3fc9dad6e85
Patryk Nowak <patryk.nowak2@deutschepost.de>
2024-11-28 14:08:35 +0100

    Conversion with custom property editor.

--- src/main/java/org/nowpat/batchtest/job/JobConfiguration.java
+++ src/main/java/org/nowpat/batchtest/job/JobConfiguration.java
@@ -1,14 +1,9 @@
 package org.nowpat.batchtest.job;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Map;
 import lombok.AllArgsConstructor;
-import org.nowpat.batchtest.conversion.CustomConverterPersonMapper;
 import org.nowpat.batchtest.conversion.DatePropertyEditor;
-import org.nowpat.batchtest.conversion.PersonFieldSetMapper;
-import org.nowpat.batchtest.conversion.StringToLocalDateConverterDayFirst;
 import org.nowpat.batchtest.model.Person;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -18,15 +13,9 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
-import org.springframework.batch.item.file.transform.FieldSetFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.io.Resource;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -57,28 +46,14 @@ public class JobConfiguration {
 
     @Bean
     public FlatFileItemReader<Person> flatFilePersonReader(@Value("${inputFile}") Resource inputFile,
-                                                           DatePropertyEditor datePropertyEditor,
-                                                           PersonFieldSetMapper personFieldSetMapper,
-                                                           CustomConverterPersonMapper customConverterPersonMapper) {
+                                                           DatePropertyEditor datePropertyEditor) {
         return new FlatFileItemReaderBuilder<Person>()
                 .name("flatFilePersonReader")
                 .resource(inputFile)
                 .delimited()
                 .names("id", "name", "dob")
-
-//                .customEditors(Map.of(LocalDate.class, datePropertyEditor))
-//                .targetType(Person.class)
-
-//                .fieldSetMapper(fieldSet -> Person.builder()
-//                        .id(fieldSet.readInt("id"))
-//                        .name(fieldSet.readString(1))
-//                        .dob(Instant.ofEpochMilli(fieldSet.readDate("dob", "dd.MM.yyyy").getTime()).atZone(ZoneId.systemDefault()).toLocalDate())
-//                        .build())
-
-//                .fieldSetMapper(personFieldSetMapper)
-
-                .fieldSetMapper(customConverterPersonMapper)
-
+                .customEditors(Map.of(LocalDate.class, datePropertyEditor))
+                .targetType(Person.class)
                 .build();
     }
 }
